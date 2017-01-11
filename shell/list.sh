166b4677f09c1ca957125c4ca032eee80f152a58
haipo yang <yang@haipo.me>
2017-01-11 10:51:43 +0800

    update list

--- shell/list.sh
+++ shell/list.sh
@@ -4,3 +4,7 @@ echo "list" | nc -q1 127.0.0.1  9001
 echo "list" | nc -q1 127.0.0.1  9002 | awk 'NR != 1'
 echo "list" | nc -q1 127.0.0.1  9003 | awk 'NR != 1'
 echo "list" | nc -q1 127.0.0.1  9004 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  9005 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  9006 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  9007 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  9008 | awk 'NR != 1'
