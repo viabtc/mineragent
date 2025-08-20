ed4ff08e5b91ddc13ef2f73a40c9b0ed4a11a60c
polar-li <polar.li@vinotech.com>
2025-08-20 07:41:28 +0000

    windows版本更新：增强系统稳定性，增加开机启动，增加返回配置逻辑

--- linux/btc_mineragent/shell/list.sh
+++ linux/btc_mineragent/shell/list.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "list" | nc -q1 127.0.0.1  9001
-echo "list" | nc -q1 127.0.0.1  9002 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9003 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9004 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9005 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9006 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9007 | awk 'NR != 1'
-echo "list" | nc -q1 127.0.0.1  9008 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8001
+echo "list" | nc -q1 127.0.0.1  8002 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8003 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8004 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8005 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8006 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8007 | awk 'NR != 1'
+echo "list" | nc -q1 127.0.0.1  8008 | awk 'NR != 1'
